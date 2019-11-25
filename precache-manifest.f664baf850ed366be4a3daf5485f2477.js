6f1a4b41e9454770b0509af74490fce8df4ae4af
Bitting <rachel.bitting@redcross.org>
2019-11-25 12:20:32 -0500

    Updates

---
+++ precache-manifest.f664baf850ed366be4a3daf5485f2477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5754552283a1a3d9ea4befca367a6a",
+    "url": "/ada-dashboard/index.html"
+  },
+  {
+    "revision": "7ca0d1362209bc6637fe",
+    "url": "/ada-dashboard/static/css/main.b998d8fd.chunk.css"
+  },
+  {
+    "revision": "8a8621cd2b8605929ad9",
+    "url": "/ada-dashboard/static/js/2.15033c3e.chunk.js"
+  },
+  {
+    "revision": "7ca0d1362209bc6637fe",
+    "url": "/ada-dashboard/static/js/main.172f9859.chunk.js"
+  },
+  {
+    "revision": "6a71bed043e6d54b9f6b",
+    "url": "/ada-dashboard/static/js/runtime-main.10314e13.js"
+  }
+]);
