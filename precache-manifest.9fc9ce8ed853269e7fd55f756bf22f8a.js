69d178a4ef4c72c5adcb8c24739ae7314d33eebc
Rachel Bitting <rachel.bitting@redcross.org>
2020-02-06 20:03:11 -0500

    Updates

---
+++ precache-manifest.9fc9ce8ed853269e7fd55f756bf22f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032a3b5f905778ef9f7c444c43b8a1eb",
+    "url": "/arc-accessibility-dashboard/index.html"
+  },
+  {
+    "revision": "bc5a4c7d9dcd8c6d51fb",
+    "url": "/arc-accessibility-dashboard/static/css/main.e9471b9e.chunk.css"
+  },
+  {
+    "revision": "811b81db0ca9d16a6c5b",
+    "url": "/arc-accessibility-dashboard/static/js/2.ae039c23.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/arc-accessibility-dashboard/static/js/2.ae039c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5a4c7d9dcd8c6d51fb",
+    "url": "/arc-accessibility-dashboard/static/js/main.a8a6be9b.chunk.js"
+  },
+  {
+    "revision": "d1ce73564560a512c612",
+    "url": "/arc-accessibility-dashboard/static/js/runtime-main.2f201635.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arc-accessibility-dashboard/precache-manifest.7ddf19920f7f5d3ec014e91c5150920d.js"
+  "/arc-accessibility-dashboard/precache-manifest.9fc9ce8ed853269e7fd55f756bf22f8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arc-accessibility-dashboard/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
